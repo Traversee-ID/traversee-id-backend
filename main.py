0aa913b9a30738202f6b8998255984815936f915
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-05-24 18:47:34 +0700

    Add sqlachemy configuration

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from firebase_admin import auth, initialize_app
 from flask import Flask
 from credentials import credentials
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
 
+db = SQLAlchemy(app)
 firebase = initialize_app(credentials)
 
 @app.route("/", methods=["GET"])
@@ -19,4 +23,6 @@ def user_info(id):
         return {"message": "User not found!"}, 404
     
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=8080)
