93936357e0a4acba8342c2f801e308492f845b04
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-05-27 01:48:37 +0700

    Add endpoint get all category and get category by id

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from firebase_admin import auth, initialize_app
 from flask import Flask
 from credentials import credentials
@@ -22,6 +23,26 @@ def user_info(id):
     except:
         return {"message": "User not found!"}, 404
     
+@dataclass
+class Category(db.Model):
+    __tablename__ = "categories"
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(100), unique=True, nullable=False)
+    image_url: str = db.Column(db.String, nullable=False)
+
+@app.route("/categories", methods=["GET"])
+def get_categories():
+    categories = db.session.query(Category).order_by(Category.name.asc()).all()
+    return {"data": categories}, 200
+
+@app.route("/categories/<int:id>", methods=["GET"])
+def get_category(id):
+    category = db.session.get(Category, id)
+    if not category:
+        return {"message": f"Category with id {id} not found!"}, 404
+    return {"data": category}, 200
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
