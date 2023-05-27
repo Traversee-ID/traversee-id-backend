a055ed12e37c64e20f5c5ce00e7a7f0fc4e73cea
imamazka <imam.azka@ui.ac.id>
2023-05-27 17:01:42 +0700

    Update comment, likes

--- main.py
+++ main.py
@@ -36,18 +36,19 @@ class Comment(db.Model):
     author: int = db.Column(db.Integer, nullable=False)
     forum: int = db.Column(db.Integer, db.ForeignKey('forum.forum_id'), nullable=False)
 
-    def __init__(self, text, author):
+    def __init__(self, text, author, forum):
         self.text = text
         self.author = author
+        self.forum = forum
 
 
-#show all forum posts
+#show all forum forums
 @app.route("/forums")
 def show_forum():
     forums = db.session.query(Forum).order_by(Forum.created_at.desc()).all()
     return {"data": forums}, 200
 
-#create new post
+#create new forum
 @app.route("/forums/create", methods = ['POST'])
 def create_forum():
     content_type = request.headers.get('Content-Type')
@@ -60,7 +61,7 @@ def create_forum():
     else:
         return {"message": f"Forum failed to create, or content-type not supported!"}, 404
 
-#get specific post detail
+#get specific forum detail
 @app.route('/forums/<int:id>')
 def get_forum(id):
     forum = db.session.get(Forum, id)
@@ -68,7 +69,7 @@ def get_forum(id):
         return {"message": f"Forum with id {id} doesn't exist"}, 404
     return {"data": forum}, 200
 
-#delete specific post
+#delete specific forum
 @app.route('/forums/<int:id>/delete', methods=["DELETE"])
 def delete_forum(id):
     forum = Forum.query.filter_by(forum_id=id).first()
@@ -77,6 +78,48 @@ def delete_forum(id):
         db.session.commit()
         return {"message": f"Forum with id {id} deleted"}, 200
     return {"message": f"Forum with id {id} doesn't exist"}, 404
+
+#give forum a like
+@app.route('/forums/<int:id>/like', methods=["PUT"])
+def like_forum(id):
+    forum = Forum.query.filter_by(forum_id=id).first()
+    if forum:
+        forum.likes += 1
+        db.session.commit()
+        return {"message": f"Added like to forum with id {id}"}, 200
+    return {"message": f"Forum with id {id} doesn't exist"}, 404
+
+#show and add comment to forum
+@app.route('/forums/<int:id>/comment', methods=["GET", "POST"])
+def comment_forum(id):
+    forum = Forum.query.filter_by(forum_id=id).first()
+
+    if request.method == 'GET' and forum:
+        comments = Comment.query.filter_by(forum=id).all()
+        return {"data": comments}, 200
+
+    if request.method == 'POST' and forum:
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            data = request.get_json()
+            newComment = Comment(data['text'], data['author'], id)
+            db.session.add(newComment)
+            db.session.commit()
+            return {"message": f"Comment added to forum with id {id}"}, 200
+        else:
+            return {"message": f"Comment failed to create!"}, 404
+
+    return {"message": f"Forum with id {id} doesn't exist!"}, 404
+
+#delete specific comment
+@app.route('/forums/<int:id>/<int:comment_id>', methods=["DELETE"])
+def delete_comment(id, comment_id):
+    comment = Comment.query.filter_by(comment_id=comment_id).first()
+    if comment:
+        db.session.delete(comment)
+        db.session.commit()
+        return {"message": f"Comment with id {comment_id} deleted"}, 200
+    return {"message": f"Comment with id {comment_id} doesn't exist"}, 404
     
 if __name__ == '__main__':
     with app.app_context():
