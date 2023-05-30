3910bacff343f65d2b304493229a9b481465cc3a
imamazka <imam.azka@ui.ac.id>
2023-05-30 12:51:40 +0700

    Add pagination in forums and comments

--- main.py
+++ main.py
@@ -15,48 +15,69 @@ firebase = initialize_app(credentials)
 
 @dataclass
 class Forum(db.Model):
+    __tablename__ = "forums"
 
-    forum_id: int = db.Column(db.Integer, primary_key=True)
+    total_likes: int
+
+    id: int = db.Column(db.Integer, primary_key=True)
     title: str = db.Column(db.String(100), nullable=False)
     text: str = db.Column(db.String(500), nullable=False)
-    created_at: str = db.Column(db.DateTime, default=db.func.now())
-    author: str = db.Column(db.String, nullable=False)
-    comments = db.relationship('Comment', lazy=True)
-    likes: int = db.Column(db.Integer, default = 0)
+    author_id: str = db.Column(db.String, nullable=False)
     image_url: str = db.Column(db.String)
+    comments = db.relationship('Comment', uselist=True)
+    likes = db.relationship('ForumLike', uselist=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
 
-    def __init__(self, title, text, author):
-        self.title = title
-        self.text = text
-        self.author = author
+    @property
+    def total_likes(self):
+        return len(self.likes)
+    
+
+@dataclass
+class ForumLike(db.Model):
+    __tablename__ = "forum_likes"
+
+    forum_id: int = db.Column(db.ForeignKey('forums.id'), primary_key=True)
+    user_id: str = db.Column(db.String, primary_key=True)
 
 @dataclass
 class Comment(db.Model):
+    __tablename__ = "comments"
 
-    comment_id: int = db.Column(db.Integer, primary_key=True)
+    id: int = db.Column(db.Integer, primary_key=True)
     text: str = db.Column(db.String(300), nullable=False)
-    author: str = db.Column(db.String, nullable=False)
-    forum: int = db.Column(db.Integer, db.ForeignKey('forum.forum_id'), nullable=False)
-
-    def __init__(self, text, author, forum):
-        self.text = text
-        self.author = author
-        self.forum = forum
+    author_id: str = db.Column(db.String, nullable=False)
+    forum_id: int = db.Column(db.Integer, db.ForeignKey('forums.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
 
 
 @app.route("/forums")
-def show_forum():
-    forums = db.session.query(Forum).order_by(Forum.created_at.desc()).all()
+def get_forums():
+    page = request.args.get("page")
+
+    if page is not None and page.isdecimal():
+        forums = db.session.query(Forum) \
+            .order_by(Forum.created_at.desc()) \
+            .paginate(page=int(page), per_page=5, error_out=False).items
+
+    else:
+        forums = db.session.query(Forum).order_by(Forum.created_at.desc()).all()
+    
     return {"data": forums}, 200
 
 @app.route("/forums", methods = ['POST'])
 @authenticated_only
 def create_forum():
-    data = request.get_json()
-    newForum = Forum(data['title'], data['text'], request.user.get('user_id'))
-    db.session.add(newForum)
+    title = request.json.get("title")
+    text = request.json.get("text")
+
+    if not title or not text:
+        return {"message": "Title and text are required"}, 400
+    
+    forum = Forum(title=title, text=text, author_id=request.user.get('user_id'))
+    db.session.add(forum)
     db.session.commit()
-    return {"message": f"Forum created"}, 200
+    return {"data": forum}, 200
 
 @app.route('/forums/<int:id>')
 def get_forum(id):
@@ -68,51 +89,105 @@ def get_forum(id):
 @app.route('/forums/<int:id>', methods=["DELETE"])
 @authenticated_only
 def delete_forum(id):
-    forum = Forum.query.filter_by(forum_id=id).first()
-    if forum:
-        db.session.delete(forum)
-        db.session.commit()
-        return {"message": f"Forum with id {id} deleted"}, 200
-    return {"message": f"Forum with id {id} doesn't exist"}, 404
+    forum = db.session.get(Forum, id)
+    if not forum:
+        return {"message": f"Forum with id {id} doesn't exist"}, 404
+    
+    if forum.author_id != request.user.get("user_id"):
+        return {"message": f"You're not author of the forum with id {id}"}, 403
+    
+    db.session.delete(forum)
+    db.session.commit()
+    return {"message": f"Forum with id {id} deleted"}, 200
 
-@app.route('/forums/<int:id>/likes', methods=["PUT"])
+@app.route('/forums/<int:id>/likes', methods=["POST"])
 @authenticated_only
-def like_forum(id):
-    forum = Forum.query.filter_by(forum_id=id).first()
-    if forum:
-        forum.likes += 1
-        db.session.commit()
-        return {"message": f"Added like to forum with id {id}"}, 200
-    return {"message": f"Forum with id {id} doesn't exist"}, 404
+def add_forum_likes(id):
+    forum = db.session.get(Forum, id)
+    if not forum:
+        return {"message": f"Forum with id {id} doesn't exist"}, 404
+    
+    user_id = request.user.get("user_id")
+    forum_likes = db.session.get(ForumLike, (forum.id, user_id))
+    if forum_likes:
+        return {"message": f"Forum {id} is already liked"}, 409
+    
+    forum_likes = ForumLike(forum_id=forum.id, user_id=user_id)
+    db.session.add(forum_likes)
+    db.session.commit()
+    return {"data": forum}, 200
+    
 
-@app.route('/forums/<int:id>/comments', methods=["GET", "POST"])
+@app.route('/forums/<int:id>/likes', methods=["DELETE"])
 @authenticated_only
-def comment_forum(id):
-    forum = Forum.query.filter_by(forum_id=id).first()
+def delete_forum_likes(id):
+    forum = db.session.get(Forum, id)
+    if not forum:
+        return {"message": f"Forum with id {id} doesn't exist"}, 404
+    
+    user_id = request.user.get("user_id")
+    forum_likes = db.session.get(ForumLike, (forum.id, user_id))
+    if not forum_likes:
+        return {"message": f"Forum {id} is not liked yet"}, 409
+    
+    db.session.delete(forum_likes)
+    db.session.commit()
+    return {"data": forum}, 200
 
-    if request.method == 'GET' and forum:
-        comments = Comment.query.filter_by(forum=id).all()
-        return {"data": comments}, 200
+@app.route('/forums/<int:id>/comments', methods=["POST"])
+@authenticated_only
+def create_forum_comments(id):
+    forum = db.session.get(Forum, id)
+    if not forum:
+        return {"message": f"Forum with id {id} doesn't exist"}, 404
 
-    if request.method == 'POST' and forum:
-        data = request.get_json()
-        newComment = Comment(data['text'], request.user.get('user_id'), id)
-        db.session.add(newComment)
-        db.session.commit()
-        return {"message": f"Comment added to forum with id {id}"}, 200
+    text = request.json.get("text")
+    if not text:
+        return {"message": "Text are required"}, 400
 
-    return {"message": f"Forum with id {id} doesn't exist!"}, 404
+    comments = Comment(text=text, author_id=request.user.get('user_id'), forum_id=forum.id)
+    db.session.add(comments)
+    db.session.commit()
+    return {"data": comments}, 200
+
+@app.route('/forums/<int:id>/comments', methods=["GET"])
+@authenticated_only
+def get_forum_comments(id):
+    page = request.args.get("page")
+    forum = db.session.get(Forum, id)
+
+    if page is not None and page.isdecimal():
+        comments = db.session.query(Comment) \
+            .filter_by(forum_id=forum.id) \
+            .order_by(Comment.created_at.desc()) \
+            .paginate(page=int(page), per_page=10, error_out=False).items
+
+    else:
+        comments = db.session.query(Comment) \
+            .filter_by(forum_id=forum.id) \
+            .order_by(Comment.created_at.desc()).all()
+
+    return {"data": comments}, 200
 
 @app.route('/forums/<int:id>/comments/<int:comment_id>', methods=["DELETE"])
 @authenticated_only
-def delete_comment(id, comment_id):
-    comment = Comment.query.filter_by(comment_id=comment_id).first()
-    if comment:
-        db.session.delete(comment)
-        db.session.commit()
-        return {"message": f"Comment with id {comment_id} deleted"}, 200
-    return {"message": f"Comment with id {comment_id} doesn't exist"}, 404
+def delete_forum_comment(id, comment_id):
+    forum = db.session.get(Forum, id)
+    if not forum:
+        return {"message": f"Forum with id {id} doesn't exist"}, 404
     
+    comment = db.session.get(Comment, comment_id)
+    if not comment:
+        return {"message": f"Comment with id {id} doesn't exist"}, 404
+    
+    if comment.author_id != request.user.get("user_id"):
+        return {"message": f"You're not author of the comment with id {id}"}, 403
+    
+    db.session.delete(comment)
+    db.session.commit()
+    return {"message": f"Comment with id {comment_id} deleted"}, 200
+
+   
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
