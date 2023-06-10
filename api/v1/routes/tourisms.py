387f82e06d214a78b217330933f2b479d1339e03
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-06-10 11:36:56 +0700

    Update temporary response tourism

--- api/v1/routes/tourisms.py
+++ api/v1/routes/tourisms.py
@@ -148,6 +148,6 @@ def get_tourism_recomendations():
 
     response = requests.post(url, json={"user_id": user_id})
     if response.status_code == 200:
-        return response.json(), 200
-    
-    return {"message": "Failed"}, 500
+        tourism = db.session.query(Tourism).paginate(page=1, per_page=10, error_out=False)
+        return {"data": Tourism.serialize_list(user_id, tourism)}, 200
+    return {"data": []}, 200
