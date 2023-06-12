8795f1112bb621be3b58986b1afe91769cbe8992
Muhammad Fikri Haryanto <mfharyantooo@gmail.com>
2023-06-12 20:39:25 +0700

    Delete endpoint get campaign category by id

--- api/v1/routes/campaigns.py
+++ api/v1/routes/campaigns.py
@@ -219,11 +219,3 @@ def get_campaign_categories():
     categories = db.session.query(CampaignCategory) \
         .order_by(CampaignCategory.name.asc()).all()
     return {"data": categories}, 200
-
-@campaigns.route("/campaign-categories/<int:id>", methods=["GET"])
-@authenticated_only
-def get_campaign_category(id):
-    category = db.session.get(CampaignCategory, id)
-    if not category:
-        return {"message": f"Category with id {id} doesn't exist"}, 404
-    return {"data": category}, 200
